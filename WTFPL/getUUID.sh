ceb34532eaaceb9e979bda85771a4e03e23aacb8
tdjnodj <108401163+tdjnodj@users.noreply.github.com>
2023-03-05 16:15:52 +0800

    Creat getUUID.sh

---
+++ WTFPL/getUUID.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+getUUID() {
+    echo ""
+    uuid_regex='^[[:xdigit:]]{8}-[[:xdigit:]]{4}-[[:xdigit:]]{4}-[[:xdigit:]]{4}-[[:xdigit:]]{12}$'
+    read -p "请输入你的 uuid: " answer
+    if [ -z "$answer" ]; then
+        uuid=$(uuidgen)
+    elif [ "$answer" == "${uuid_regex}" ]; then
+        uuid="${answer}"
+    else
+        red "uuid 不合法！"
+        exit 1
+    fi
+}
